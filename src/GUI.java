2ad0ea966161464110705c39e5a20b353394ff9d
Flangrys <63200230+Flangrys@users.noreply.github.com>
2023-03-03 18:31:20 -0300

    Initial commit

---
+++ src/GUI.java
@@ -0,0 +1,25 @@
+import javax.swing.*;
+
+public class GUI {
+    private JPanel Container;
+    private JButton B1;
+    private JButton B2;
+    private JButton B3;
+    private JButton B4;
+    private JButton B6;
+    private JButton B5;
+    private JButton B7;
+    private JButton B8;
+    private JButton B9;
+    private JButton B0;
+    private JLabel ExpressionBox;
+    private JLabel ResultBox;
+    private JButton BMultiply;
+    private JButton BDivision;
+    private JButton BAdd;
+    private JButton BSub;
+    private JLabel AppTitle;
+    private JButton BEquals;
+    private JLabel ExpressionText;
+    private JLabel ResultText;
+}
